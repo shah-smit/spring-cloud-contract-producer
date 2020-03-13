1473db812be1b12bc18f6e06464e5fade2225f4d
Smit <smitshah95@hotmail.com>
2020-03-13 15:14:41 +0800

    first commit

---
+++ src/test/java/com/example/cloudcontract/CloudContractApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cloudcontract;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudContractApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/test/resources/contracts/com.example.cloudcontract/shouldReturnOddWhenRequestParamIsOdd.groovy
@@ -0,0 +1,16 @@
+import org.springframework.cloud.contract.spec.Contract
+Contract.make {
+    description "should return odd when number input is odd"
+    request{
+        method GET()
+        url("/validate/prime-number") {
+            queryParameters {
+                parameter("number", "3")
+            }
+        }
+    }
+    response {
+        body("Odd")
+        status 200
+    }
+}
